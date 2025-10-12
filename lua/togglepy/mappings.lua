97b20e7cc1085f9ec2308ab0c574f8dccd00bd6a
MvdS <postaanmartin@gmail.com>
2025-10-12 10:32:32 +0200

    minor

--- lua/togglepy/mappings.lua
+++ lua/togglepy/mappings.lua
@@ -8,6 +8,7 @@ function M.setup(opts)
 		pattern = "python",
 		callback = function()
 			if opts.window_navigation then
+				-- Make switching between windows work in terminal mode
 				vim.keymap.set("t", "<C-w>h", "<C-\\><C-n><C-w>h", { noremap = true })
 				vim.keymap.set("t", "<C-w>j", "<C-\\><C-n><C-w>j", { noremap = true })
 				vim.keymap.set("t", "<C-w>k", "<C-\\><C-n><C-w>k", { noremap = true })
