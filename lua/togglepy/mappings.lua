272360114d58d35ba1f308ce9b970669b886a292
MvdS <postaanmartin@gmail.com>
2025-10-12 10:14:06 +0200

    Set default options window navigation

--- lua/togglepy/mappings.lua
+++ lua/togglepy/mappings.lua
@@ -1,12 +1,13 @@
 local M = {}
 
 function M.setup(opts)
-	opts = opts or {}
+	-- Set default options
+	opts = vim.tbl_deep_extend("force", { window_navigation = true }, opts or {})
+	-- Key mappings for navigating between Python buffers and toggply's IPython terminal
 	vim.api.nvim_create_autocmd("FileType", {
 		pattern = "python",
 		callback = function()
-			local window_navigation = opts.window_navigation or true
-			if window_navigation then
+			if opts.window_navigation then
 				vim.keymap.set("t", "<C-w>h", "<C-\\><C-n><C-w>h", { noremap = true })
 				vim.keymap.set("t", "<C-w>j", "<C-\\><C-n><C-w>j", { noremap = true })
 				vim.keymap.set("t", "<C-w>k", "<C-\\><C-n><C-w>k", { noremap = true })
