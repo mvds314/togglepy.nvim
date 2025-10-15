759b96c2b2f89fcecb55b35f9d9eb98e49769e3b
MvdS <m.schans@robeco.nl>
2025-10-15 18:06:21 +0200

    Pass opts to mappings

--- lua/togglepy/init.lua
+++ lua/togglepy/init.lua
@@ -5,19 +5,20 @@ local _setup_called = false
 function M.setup(opts)
 	_setup_called = true
 	local ipdab_opts = opts and opts.ipdab or {}
+	local repl_opts = opts and opts.repl or {}
+	local mapping_opts = opts and opts.mappings or {}
 	-- local repl_opts = opts and opts.repl or {}
 	require("togglepy.ipdab").setup(ipdab_opts)
 	require("togglepy.repl")
-	-- Pass the mapping opts
-	require("togglepy.mappings").setup()
+		-- Pass the mapping opts
+		.require("togglepy.mappings")
+		.setup(mapping_opts)
 end
 
 -- Auto-setup with defaults if setup() wasn't called explicitly
 vim.schedule(function()
 	if not _setup_called then
-		require("togglepy.ipdab").setup()
-		require("togglepy.repl")
-		require("togglepy.mappings").setup()
+		M.setup()
 	end
 end)
 
