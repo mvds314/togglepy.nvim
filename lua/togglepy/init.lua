ae1c0a98cea9112028baa7fae1c4e573ee694f7f
MvdS <postaanmartin@gmail.com>
2025-10-26 09:34:46 +0100

    Analysis op the problem

--- lua/togglepy/init.lua
+++ lua/togglepy/init.lua
@@ -3,7 +3,12 @@ local M = {}
 local _setup_called = false
 
 function M.setup(opts)
-	_setup_called = true
+	if _setup_called then
+		vim.notify("togglepy.nvim: setup() called again, should not happen", vim.log.levels.WARN)
+	else
+		_setup_called = true -- Prevent race condition
+		vim.notify("togglepy.nvim: calling setup", vim.log.levels.INFO)
+	end
 	opts = opts or {}
 	local ipdab_opts = opts.ipdab or {}
 	local repl_opts = opts.repl or {}
@@ -15,12 +20,15 @@ function M.setup(opts)
 	require("togglepy.keymaps").setup(keymap_opts)
 end
 
+-- TODO: this seems to introduce a race condition, how to deal with it properly?, probably due to lazy loading
+
 -- Auto-setup with defaults if setup() wasn't called explicitly
-vim.schedule(function()
-	if not _setup_called then
-		M.setup()
-	end
-end)
+-- vim.schedule(function()
+-- 	if not _setup_called then
+-- 		vim.notify("togglepy.nvim: setup() not called, using default options", vim.log.levels.INFO)
+-- 		M.setup()
+-- 	end
+-- end)
 
 -- TODO: update documentation for the plugin
 
