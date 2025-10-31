31af22f5e6c56dd13d83bfc7a3f8acf73de29b3c
mvds314 <mvds314@users.noreply.github.com>
2025-10-31 20:17:44 +0100

    Store current config logic

--- lua/togglepy/ipdab.lua
+++ lua/togglepy/ipdab.lua
@@ -1,30 +1,12 @@
 local M = {}
 
-local current_dapui_config
+local original_dapui_config
 local adjusted_dapui_config
 
-function M.setup(opts)
-	-- Default options
-	opts = vim.tbl_deep_extend("force", {
-		host = "localhost",
-		port = 9000,
-	}, opts or {})
-	opts = opts or {}
-	local dap_ok, dap = pcall(require, "dap")
-	if not dap_ok then
-		vim.notify("nvim-dap is required for togglepy.nvim", vim.log.levels.ERROR)
-		return
-	end
-
-	local dapui_ok, dapui = pcall(require, "dapui")
-	if not dapui_ok then
-		vim.notify("nvim-dap-ui is required for togglepy.nvim", vim.log.levels.ERROR)
-		return
-	end
-
-	-- Save the original dapui configuration
+local function copy_dapui_config()
 	local dapui_config = require("dapui.config")
 	-- Note we have the fields of dapui_config hardcoded here, as they cannot be retrieved programmatically
+	local copy_of_config
 	for _, f in ipairs({
 		"icons",
 		"mappings",
@@ -36,24 +18,62 @@ function M.setup(opts)
 		"controls",
 		"render",
 	}) do
-		current_dapui_config = current_dapui_config or {}
-		current_dapui_config[f] = vim.deepcopy(dapui_config[f])
+		copy_of_config = copy_of_config or {}
+		copy_of_config[f] = vim.deepcopy(dapui_config[f])
 	end
-	adjusted_dapui_config = vim.deepcopy(current_dapui_config)
-	adjusted_dapui_config.layouts.elements = {}
+	return copy_of_config
+end
 
-	-- TODO: continue here
+local function reset_dapui()
+	local dapui = require("dapui")
+	if original_dapui_config ~= nil then
+		dapui.setup(original_dapui_config)
+		original_dapui_config = nil
+	end
+	dapui.close()
+end
+
+function M.setup(opts)
+	-- Default options
+	opts = vim.tbl_deep_extend("force", {
+		host = "localhost",
+		port = 9000,
+	}, opts or {})
+	opts = opts or {}
+	-- Require dap
+	local dap_ok, dap = pcall(require, "dap")
+	if not dap_ok then
+		vim.notify("nvim-dap is required for togglepy.nvim", vim.log.levels.ERROR)
+		return
+	end
+	-- Require dapui
+	local dapui_ok, dapui = pcall(require, "dapui")
+	if not dapui_ok then
+		vim.notify("nvim-dap-ui is required for togglepy.nvim", vim.log.levels.ERROR)
+		return
+	end
 	-- Set listeners to change the dapui layout based on the configuration used
 	dap.listeners.after.event_initialized["dapui_config"] = function()
+		-- Store the original config globally, and temporarily switch to a modified one
 		local config = dap.session().config
-		-- Maybe put the copy logic here
 		if config.name == "Attach to ipdb (manual %run)" then
+			if original_dapui_config == nil then
+				original_dapui_config = copy_dapui_config()
+			end
+			adjusted_dapui_config = vim.deepcopy(original_dapui_config)
+			adjusted_dapui_config.layouts.elements = {}
 			dapui.setup(adjusted_dapui_config)
-		else
-			dapui.setup(current_dapui_config)
+		elseif original_dapui_config ~= nil then
+			-- Restore the original config
+			dapui.setup(original_dapui_config)
+			original_dapui_config = nil
 		end
 		dapui.open()
 	end
+
+	dap.listeners.before.event_terminated["dapui_config"] = reset_dapui
+	dap.listeners.before.event_exited["dapui_config"] = reset_dapui
+
 	dap.adapters.ipdb = {
 		type = "server",
 		host = opts.host,
