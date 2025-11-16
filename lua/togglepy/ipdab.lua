9b22b42bcfeeff1e102d5b038eb5f4edd0ff456d
MvdS <postaanmartin@gmail.com>
2025-11-16 19:31:00 +0100

    Add additional listeners for testing

--- lua/togglepy/ipdab.lua
+++ lua/togglepy/ipdab.lua
@@ -86,9 +86,92 @@ function M.setup(opts)
 				-- this seems to get executed, but the terminal still loses focus -> test separately using the commented command below
 				-- Test this with a sidepane terminal first
 				-- Then test it with a floating terminal
-				require("togglepy.repl").switch_to_ipy_term()
+				-- I don't understand why this logic is not correct
+				-- Debug all the listeners, i.e., write custom listeners, and see when the focus is lost
+				-- require("togglepy.repl").switch_to_ipy_term()
 				repl.switch_to_ipy_term()
 			end)
+		end
+	end
+	-- TODO: remove these listeners after testing
+	dap.listeners.after.event_continued["ipdab_check_terminal_focus"] = function()
+		local has_focus = require("togglepy.repl").ipy_term_has_focus()
+		if has_focus then
+			vim.notify("Continued event received, terminal has focus")
+		else
+			vim.notify("Continued event received, terminal does not have focus")
+		end
+	end
+	dap.listeners.before.event_continued["ipdab_check_terminal_focus"] = function()
+		local has_focus = require("togglepy.repl").ipy_term_has_focus()
+		if has_focus then
+			vim.notify("Before Continued event received, terminal has focus")
+		else
+			vim.notify("Before Continued event received, terminal does not have focus")
+		end
+	end
+	dap.listeners.before.event_stopped["ipdab_check_terminal_focus"] = function()
+		local has_focus = require("togglepy.repl").ipy_term_has_focus()
+		if has_focus then
+			vim.notify("Before Stopped event received, terminal has focus")
+		else
+			vim.notify("Before Stopped event received, terminal does not have focus")
+		end
+	end
+	dap.listeners.after.event_stopped["ipdab_check_terminal_focus"] = function()
+		local has_focus = require("togglepy.repl").ipy_term_has_focus()
+		if has_focus then
+			vim.notify("After Stopped event received, terminal has focus")
+		else
+			vim.notify("After Stopped event received, terminal does not have focus")
+		end
+	end
+	dap.listeners.before.event_module["ipdab_check_terminal_focus"] = function()
+		local has_focus = require("togglepy.repl").ipy_term_has_focus()
+		if has_focus then
+			vim.notify("Before Module event received, terminal has focus")
+		else
+			vim.notify("Before Module event received, terminal does not have focus")
+		end
+	end
+	dap.listeners.after.event_module["ipdab_check_terminal_focus"] = function()
+		local has_focus = require("togglepy.repl").ipy_term_has_focus()
+		if has_focus then
+			vim.notify("After Module event received, terminal has focus")
+		else
+			vim.notify("After Module event received, terminal does not have focus")
+		end
+	end
+	dap.listeners.before.event_thread["ipdab_check_terminal_focus"] = function()
+		local has_focus = require("togglepy.repl").ipy_term_has_focus()
+		if has_focus then
+			vim.notify("Before Thread event received, terminal has focus")
+		else
+			vim.notify("Before Thread event received, terminal does not have focus")
+		end
+	end
+	dap.listeners.after.event_thread["ipdab_check_terminal_focus"] = function()
+		local has_focus = require("togglepy.repl").ipy_term_has_focus()
+		if has_focus then
+			vim.notify("After Thread event received, terminal has focus")
+		else
+			vim.notify("After Thread event received, terminal does not have focus")
+		end
+	end
+	dap.listeners.before.event_output["ipdab_check_terminal_focus"] = function()
+		local has_focus = require("togglepy.repl").ipy_term_has_focus()
+		if has_focus then
+			vim.notify("Before Output event received, terminal has focus")
+		else
+			vim.notify("Before Output event received, terminal does not have focus")
+		end
+	end
+	dap.listeners.after.event_output["ipdab_check_terminal_focus"] = function()
+		local has_focus = require("togglepy.repl").ipy_term_has_focus()
+		if has_focus then
+			vim.notify("After Output event received, terminal has focus")
+		else
+			vim.notify("After Output event received, terminal does not have focus")
 		end
 	end
 
