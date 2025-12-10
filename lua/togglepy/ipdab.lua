3d3857feb3304a4d860a88ea57f0d9a5fe57d4b3
MvdS <postaanmartin@gmail.com>
2025-12-10 22:21:21 +0100

    Some attemsp to fix the issue

--- lua/togglepy/ipdab.lua
+++ lua/togglepy/ipdab.lua
@@ -32,7 +32,7 @@ function M.setup(opts)
 				-- Debug all the listeners, i.e., write custom listeners, and see when the focus is lost
 				-- What I also don't understand is that on each break this code seems to run twice -> this is due to double stop notification
 				-- require("togglepy.repl").switch_to_ipy_term()
-				repl.switch_to_ipy_term()
+				-- repl.switch_to_ipy_term()
 			end)
 		end
 	end
@@ -117,6 +117,46 @@ function M.setup(opts)
 			vim.notify("After Output event received, terminal does not have focus")
 		end
 	end
+
+	dap.listeners.after.event_initialized["ipdab_check_terminal_focus"] = function(mysession)
+		-- vim.notify(vim.inspect(mysession), vim.log.levels.INFO)
+		-- vim.notify(vim.inspect(getmetatable(mysession)), vim.log.levels.INFO)
+		-- Override the jump_to_frame function
+		-- local session_mt = getmetatable(mysession)
+
+		local session_mt = getmetatable(dap.session())
+		-- Continue here
+		-- Plan is to override mysession.listeners.after.event_stopped -> make sure my own logic runs as planned
+
+	-- 	if session_mt then
+	-- 		local original_jump_to_frame = session_mt.__index.jump_to_frame -- Save the original function
+	--
+	-- 		-- Override the jump_to_frame function globally
+	-- 		session_mt.__index.jump_to_frame = function(self, frame, preserve_focus_hint, stopped)
+	-- 			vim.notify("Executing overridden jump_to_frame")
+	--
+	-- 			-- Save the current window and cursor position
+	-- 			local current_win = vim.api.nvim_get_current_win()
+	-- 			local current_buf = vim.api.nvim_get_current_buf()
+	-- 			local current_cursor = vim.api.nvim_win_get_cursor(current_win)
+	--
+	-- 			-- Call the original jump_to_frame function
+	-- 			original_jump_to_frame(self, frame, preserve_focus_hint, stopped)
+	-- 			-- Restore the cursor to the original window and position
+	-- 			if vim.api.nvim_win_is_valid(current_win) and vim.api.nvim_buf_is_valid(current_buf) then
+	-- 				vim.notify("Restoring focus to the original window after jump_to_frame")
+	-- 				vim.api.nvim_set_current_win(current_win)
+	-- 				vim.api.nvim_win_set_cursor(current_win, current_cursor)
+	-- 			else
+	-- 				vim.notify("Not restoring focus to the original window after jump_to_frame")
+	-- 			end
+	-- 		end
+	-- 		debug.setmetatable(dap.session(), session_mt)
+	-- 		vim.notify("Overriding metatable")
+	-- 	else
+	-- 		vim.notify("Failed to retrieve the Session metatable", vim.log.levels.ERROR)
+	-- 	end
+	-- end
 
 	dap.adapters.ipdab = {
 		type = "server",
